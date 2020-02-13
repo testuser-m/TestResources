630b155543bea7d30248ec94d63d5a2926e899cc
Mario Martínez <extern.mario.martinez@seat.es>
2020-02-13 15:40:34 +0100

    Initial commit

---
+++ TestResources/TestResources/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestResources
+//
+//  Created by Mario Martínez Fidalgo on 13/02/2020.
+//  Copyright © 2020 Mario Martínez Fidalgo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
